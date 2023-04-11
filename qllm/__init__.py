66428c0151b96d6fa7624821df44bb808572b8a2
SpringWave1 <534239589@qq.com>
2023-04-11 14:01:53 +0000

    add fp16

--- qllm/__init__.py
+++ qllm/__init__.py
@@ -5,6 +5,6 @@ from . import utils
 
 from .custom_generate import SequentialGenerate
 
-available_bits = [2, 4, 8, '8:tc', '8:tc-li']
+available_bits = [2, 4, 8, '8:tc', '8:tc-li', 16]
 def get_available_bits():
     return available_bits
